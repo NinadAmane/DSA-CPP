4eb975a8f13212dadd0e6f0a3980e1bad2b7b5bc
Ninad Amane <144771256+NinadAmane@users.noreply.github.com>
2026-08-01 15:28:14 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (85.12%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,15 +1,25 @@
 class Solution {
 public:
-    int f(int l, int r, vector<int>&nums,int n){
-        if(l==r){
-            return nums[l];
+    int f(int l, int r, int score1, int score2, bool turn, vector<int>&nums){
+        int n=nums.size();
+        if(l>r){
+            return score1>=score2;
+        }
+        if(turn){
+            return f(l+1,r,score1 + nums[l], score2, false,nums) || 
+                    f(l,r-1,score1 + nums[r], score2, false, nums);
+        }
+        else{
+            return f(l+1,r,score1 , score2 + nums[l], true,nums) &&
+                    f(l,r-1,score1 , score2 + nums[r], true, nums);
         }
 
-        return max(nums[l] - f(l+1,r, nums, n) , nums[r] - f(l,r-1,nums,n));
+        return false;
+        
     }
     bool predictTheWinner(vector<int>& nums) {
         int n= nums.size();
         
-        return f(0,n-1,nums,n) >=0;
+        return f(0,n-1,0,0,true,nums);
     }
 };
