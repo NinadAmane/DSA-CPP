498f00d1834992a28d74d1e5fe4445a80e32f0f4
Ninad Amane <144771256+NinadAmane@users.noreply.github.com>
2026-08-14 17:17:38 +0530

    Time: 0 ms (100.00%) | Memory: 94.4 MB (86.46%) - LeetSync

--- 1474-longest-zigzag-path-in-a-binary-tree/longest-zigzag-path-in-a-binary-tree.cpp
+++ 1474-longest-zigzag-path-in-a-binary-tree/longest-zigzag-path-in-a-binary-tree.cpp
@@ -11,84 +11,85 @@
  */
 class Solution {
 public:
-    
-    // int dfs(TreeNode* root, int count, bool turn){ //returns the longest zigzag route
-    //     if(root== NULL) return 0;
+    int maxi= 0;
+    void dfs(TreeNode* root, int count, bool turn){ //returns the longest zigzag route
+        if(root== NULL) return ;
         
-    //     if(turn){
-    //         if(root->left){
-    //             // count = 1;
-    //             return dfs(root->left, count, turn);
-    //         }
-    //         if(root->right){
-    //             return dfs(root->right, count + 1, !turn);
-    //         }
-    //     }
-    //     else{
-    //         if(root->left){
-    //             return dfs(root->left, count + 1, !turn);
-    //         }
-    //         if(root->right){
-    //             // count = 1;
-    //             return dfs(root->right, count, turn);
-    //         }
-    //     }
-    //     maxi = max(count, maxi);
+        maxi = max(count, maxi);
+        if(turn){
+            if(root->left){
+                // count = 1;
+                dfs(root->left, 1, turn);
+            }
+            if(root->right){
+                dfs(root->right, count + 1, !turn);
+            }
+        }
+        else{
+            if(root->left){
+                dfs(root->left, count + 1, !turn);
+            }
+            if(root->right){
+                // count = 1;
+                dfs(root->right, 1, turn);
+            }
+        }
 
-    //     return maxi;
-    // }
+        return;
+    }
     //true = left
     //false = right
 
     int longestZigZag(TreeNode* root) {
         if(root == NULL) return 0;
         
-        // int count = 1;
+        int count = 1;
 
-        // int first= 0;
-        // if(root->left) first = dfs(root->left, count , true);
-        // int second = 0;
-        // if(root->right) second = dfs(root->right,count,false);
-        // // cout << (count) << endl;
-        // return max({first, second});
-
-
-
-
-
-        stack<pair<TreeNode*, pair<int,bool>>> st; //node, count , // lastMove = true  -> previous move was LEFT
-                                                                    // lastMove = false -> previous move was RIGHT
-        int maxi = 0;
-
-        if(root->left) st.push({root->left, {1, true}});
-        if(root->right) st.push({root->right, {1, false}});
-
-        while(!st.empty()){
-            TreeNode* node = st.top().first;
-            int count = st.top().second.first;
-            bool direction = st.top().second.second;
-            st.pop();
-            maxi = max(count , maxi);
-            if(direction){
-                if(node->left){
-                    
-                    st.push({node->left, {1, direction}});
-                }
-                if(node->right){
-                    st.push({node->right, {count + 1, !direction}});
-                }
-            }
-            else{
-                if(node->left){
-                    st.push({node->left, {count +1 , !direction}});
-                }
-                if(node->right){
-                    
-                    st.push({node->right, {1, direction}});
-                }
-            }
-        }
-
+        
+        if(root->left) dfs(root->left, count , true);
+        
+        if(root->right) dfs(root->right,count,false);
+        // cout << (count) << endl;
         return maxi;
+        
+
+
+
+
+
+        // stack<pair<TreeNode*, pair<int,bool>>> st; //node, count , // lastMove = true  -> previous move was LEFT
+        //                                                             // lastMove = false -> previous move was RIGHT
+        // int maxi = 0;
+
+        // if(root->left) st.push({root->left, {1, true}});
+        // if(root->right) st.push({root->right, {1, false}});
+
+        // while(!st.empty()){
+        //     TreeNode* node = st.top().first;
+        //     int count = st.top().second.first;
+        //     bool direction = st.top().second.second;
+        //     st.pop();
+        //     maxi = max(count , maxi);
+        //     if(direction){
+        //         if(node->left){
+                    
+        //             st.push({node->left, {1, direction}});
+        //         }
+        //         if(node->right){
+        //             st.push({node->right, {count + 1, !direction}});
+        //         }
+        //     }
+        //     else{
+        //         if(node->left){
+        //             st.push({node->left, {count +1 , !direction}});
+        //         }
+        //         if(node->right){
+                    
+        //             st.push({node->right, {1, direction}});
+        //         }
+        //     }
+        // }
+
+        // return maxi;
     }
 };
