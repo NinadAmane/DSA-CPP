7a9f11d816ea20c4ffa1257c91150a98b62402b2
Ninad Amane <144771256+NinadAmane@users.noreply.github.com>
2026-09-10 19:49:56 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (49.01%) - LeetSync

--- 27-remove-element/remove-element.cpp
+++ 27-remove-element/remove-element.cpp
@@ -1,24 +1,14 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        unordered_map<int,int> freq;
+        int k =0;
         int n = nums.size();
         for(int i=0;i<n;i++){
             if(nums[i] != val){
-                freq[nums[i]]++;
-                // cout << nums[i] << " ";
+                nums[k] = nums[i];
+                k++;
             }
         }
-
-        int i=0;
-        for(auto& it : freq){
-            cout << it.first << "->" <<it.second<<" ";
-            while(it.second--){
-                nums[i] = it.first;
-                i++;
-            }
-        }
-        
-        return i;
+        return k;
     }
 };
